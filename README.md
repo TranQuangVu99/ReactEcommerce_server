e92429e3a2dbda362cbde5cf9f81034adac53f3b
Vũ Trần <tranquangvu.pis@gmail.com>
2020-12-06 17:55:25 +0700

    update server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ecommerce-Server
+
+#update get address when login
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
 const User = require("../models/user");
-const verifyToken = require('../middlewares/verify-token');
+const verifyToken = require("../middlewares/verify-token");
 const jwt = require("jsonwebtoken");
-
+const Address = require("../models/address");
 /* Signup Route*/
 router.post("/auth/signup", async (req, res) => {
-  if (!req.body.namelogin || !req.body.password ) {
-
+  if (!req.body.namelogin || !req.body.password) {
     res.json({ success: false, message: "Please enter email or password" });
   } else {
     try {
@@ -32,8 +31,10 @@ router.post("/auth/signup", async (req, res) => {
 /**Profile Route */
 router.get("/auth/user", verifyToken, async (req, res) => {
   try {
-    let foundUser =  await User.findOne({ _id: req.decoded._id }).populate('address');
-    
+    let foundUser = await User.findOne({ _id: req.decoded._id }).populate(
+      "address"
+    );
+
     if (foundUser) {
       res.json({
         success: true,
@@ -48,25 +49,25 @@ router.get("/auth/user", verifyToken, async (req, res) => {
   }
 });
 /**Update a profile */
-router.put('/auth/user',verifyToken, async(req,res)=>{
+router.put("/auth/user", verifyToken, async (req, res) => {
   try {
     let foundUser = await User.findOne({ _id: req.decoded._id });
     if (foundUser) {
-      if(req.body.name){
-        foundUser.name = req.body.name
+      if (req.body.name) {
+        foundUser.name = req.body.name;
       }
-      if(req.body.email){
-        foundUser.email = req.body.email
+      if (req.body.email) {
+        foundUser.email = req.body.email;
       }
-      if(req.body.password){
-        foundUser.password = req.body.password
+      if (req.body.password) {
+        foundUser.password = req.body.password;
       }
-      await foundUser.save()
+      await foundUser.save();
 
       res.json({
         success: true,
-        message:"Successfully updated"
-      })
+        message: "Successfully updated",
+      });
     }
   } catch (error) {
     res.status(500).json({
@@ -74,19 +75,22 @@ router.put('/auth/user',verifyToken, async(req,res)=>{
       message: error.message,
     });
   }
-})
+});
 
 /**Login route */
 router.post("/auth/login", async (req, res) => {
   try {
-    let foundUser = {}
-    if(req.body.email){
-      foundUser = await User.findOne({ email: req.body.email });
-    }
-    else{
-      if(req.body.namelogin){
-        foundUser = await User.findOne({ namelogin: req.body.namelogin });
-      }else{
+    let foundUser = {};
+    if (req.body.email) {
+      foundUser = await User.findOne({ email: req.body.email }).populate(
+        "address"
+      );
+    } else {
+      if (req.body.namelogin) {
+        foundUser = await User.findOne({
+          namelogin: req.body.namelogin,
+        }).populate("address");
+      } else {
         res.status(403).json({
           success: false,
           message: "Authentication failed, User not found",
@@ -103,24 +107,26 @@ router.post("/auth/login", async (req, res) => {
         let token = jwt.sign(foundUser.toJSON(), process.env.SECRET, {
           expiresIn: 604800, // one week
         });
+
         res.json({
-            success:true,
-            token: token,
-            email:foundUser.email,
-            username : foundUser.username
-        })
-      }else{
-          res.status(403).json({
-              success: false,
-              message:"Authentication failed, Wrong password!"
-          })
+          success: true,
+          token: token,
+          email: foundUser.email,
+          username: foundUser.username,
+          address: foundUser.address,
+        });
+      } else {
+        res.status(403).json({
+          success: false,
+          message: "Authentication failed, Wrong password!",
+        });
       }
     }
   } catch (error) {
     res.status(500).json({
-        success: false,
-        message: error.message,
-      });
+      success: false,
+      message: error.message,
+    });
   }
 });
 module.exports = router;
