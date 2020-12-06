0f306dc604837d3fefc9339f0f7a23a74253e895
Vũ Trần <tranquangvu.pis@gmail.com>
2020-12-06 09:18:46 +0700

    release

--- routes/order.js
+++ routes/order.js
@@ -6,32 +6,12 @@ const verifyToken = require("../middlewares/verify-token");
 router.get("/orders", verifyToken, async (req, res) => {
   try {
     let orders = await Order.find({ user: req.decoded._id })
-      .deepPopulate(
-        "products.productID.colors.image products.productID.colors.color products.productID.capacities"
-      )
+      .deepPopulate("products.productID")
       .exec();
-
-    let orderDTO = []
-    orders.map((order) => {
-      let orderdetails = [];
-      order.products.map((product) => {
-        const data = {
-          name: product.productID.name,
-          image: product.productID.colors[0].image.photo,
-          quantity: product.quantity,
-          price: product.price,
-        };
-        orderdetails.push(data);
-      });
-      orderDTO.push({
-        estimatedDelivery:order.estimatedDelivery,
-        orderDetail:orderdetails
-      })
-    });
 
     res.json({
       success: true,
-      orders: orderDTO,
+      orders: orders,
     });
   } catch (err) {
     res.status(500).json({
--- routes/product.js
+++ routes/product.js
@@ -94,8 +94,6 @@ function paginatedResults(model) {
   return async (req, res, next) => {
     const page = parseInt(req.query.page);
     const limit = parseInt(req.query.limit);
-    const priceFrom = req.query.priceFrom;
-    const priceTo = req.query.priceTo;
 
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
